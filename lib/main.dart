412bc93a86087027bf1564dcd98b7ada303d65be
saeedtaimoor77 <saeed.taimoor26@gmail.com>
2023-05-02 17:09:58 +0300

    Initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:motor_avenue/Home.dart';
 import 'package:motor_avenue/LiveAuction.dart';
-import 'package:motor_avenue/Settings.dart';
 import 'package:motor_avenue/car_details.dart';
 import 'package:motor_avenue/login.dart';
 import 'package:motor_avenue/splash.dart';
+import 'package:motor_avenue/store.dart';
 
 void main() => runApp(const MyApp());
 
@@ -15,10 +15,10 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       debugShowCheckedModeBanner: false,
       title: _title,
-      home: Settings(),
+      home: store(),
     );
   }
 }
