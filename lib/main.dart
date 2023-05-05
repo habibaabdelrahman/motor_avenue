f666304f424fe6cf075927b8bdd6ddbecb461147
habibaabdelrahmane <habibaabdelrahmanmosa@email.address>
2023-05-05 16:38:53 +0300

    Initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -15,10 +15,10 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       debugShowCheckedModeBanner: false,
       title: _title,
-      home: Settings(),
+      home: Home(),
     );
   }
 }
